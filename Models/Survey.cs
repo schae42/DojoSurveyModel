c17184faf2d9041f464e1c2213f0b6d4fc7df679
schae42 <chaechan@outlook.com>
2020-12-06 22:59:40 -0800

    dojo survey model

---
+++ Models/Survey.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DojoSurveyModel.Models
+{
+    public class Survey
+    {
+        public string Name { get; set; }
+        public string Locations { get; set; }
+        public string Languages { get; set; }
+        public string Comments { get; set; } 
+    }
+}
